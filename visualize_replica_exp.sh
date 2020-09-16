07f9e3c8c44769cf4230fe6b3a380d392105c443
Ronghang Hu <ronghang.hu@gmail.com>
2020-09-16 13:10:27 -0700

    add scripts to dump visualizations

---
+++ visualize_replica_exp.sh
@@ -0,0 +1,17 @@
+EXP_NAME=$1
+
+mmf_run config=projects/neural_rendering/configs/replica/${EXP_NAME}.yaml \
+    datasets=replica \
+    model=mesh_renderer \
+    env.save_dir=./save/replica/${EXP_NAME} \
+    model_config.mesh_renderer.save_forward_results=True \
+    model_config.mesh_renderer.forward_results_dir=save/visualization/${EXP_NAME}/train \
+    checkpoint.resume=True checkpoint.resume_best=True run_type=val
+
+mmf_run config=projects/neural_rendering/configs/replica/${EXP_NAME}.yaml \
+    datasets=replica \
+    model=mesh_renderer \
+    env.save_dir=./save/replica/${EXP_NAME} \
+    model_config.mesh_renderer.save_forward_results=True \
+    model_config.mesh_renderer.forward_results_dir=save/visualization/${EXP_NAME}/test \
+    checkpoint.resume=True checkpoint.resume_best=True run_type=test

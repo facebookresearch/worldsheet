78375f71df7d80926a48103f3be04a77c3061a9c
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-04 19:36:47 -0700

    new notebooks for diode visualization

--- visualize_diode_exp.sh
+++ visualize_diode_exp.sh
@@ -6,13 +6,13 @@ python mmf_cli/run.py config=projects/neural_rendering/configs/diode/${EXP_NAME}
     model=mesh_renderer \
     env.save_dir=./save/diode/${EXP_NAME} \
     model_config.mesh_renderer.save_forward_results=True \
-    model_config.mesh_renderer.forward_results_dir=save/visualization/${EXP_NAME}/val \
+    model_config.mesh_renderer.forward_results_dir=save/visualization_diode/${EXP_NAME}/val \
     checkpoint.resume=True checkpoint.resume_best=True run_type=val ${ARGS}
 
-# python mmf_cli/run.py config=projects/neural_rendering/configs/diode/${EXP_NAME}.yaml \
-#     datasets=diode \
-#     model=mesh_renderer \
-#     env.save_dir=./save/diode/${EXP_NAME} \
-#     model_config.mesh_renderer.save_forward_results=True \
-#     model_config.mesh_renderer.forward_results_dir=save/visualization/${EXP_NAME}/test \
-#     checkpoint.resume=True checkpoint.resume_best=True run_type=test ${ARGS}
+python mmf_cli/run.py config=projects/neural_rendering/configs/diode/${EXP_NAME}.yaml \
+    datasets=diode \
+    model=mesh_renderer \
+    env.save_dir=./save/diode/${EXP_NAME} \
+    model_config.mesh_renderer.save_forward_results=True \
+    model_config.mesh_renderer.forward_results_dir=save/visualization_diode/${EXP_NAME}/train \
+    checkpoint.resume=True checkpoint.resume_best=True run_type=test ${ARGS}

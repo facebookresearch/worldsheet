da671e977942a77fd66c077e6f131ee977e0c43c
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-08 13:46:21 -0700

    add new scripts for synsin eval

---
+++ run_synsin_mp3d_eval_iter.sh
@@ -0,0 +1,9 @@
+EXP_NAME=$1
+ITER=$2
+ARGS=$3
+
+python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_habitat/${EXP_NAME}.yaml \
+    datasets=synsin_habitat \
+    model=mesh_renderer \
+    env.save_dir=./save/synsin_habitat/${EXP_NAME} \
+    checkpoint.resume_file=save/synsin_habitat/${EXP_NAME}/models/model_${ITER}.ckpt run_type=test ${ARGS}

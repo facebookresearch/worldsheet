a77d0e1fb889355b95f28ea82f795fc60fc9ce98
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-09 21:39:11 -0700

    add new eval scripts for synsin_habitat

---
+++ run_synsin_mp3d_eval_val.sh
@@ -0,0 +1,8 @@
+EXP_NAME=$1
+ARGS=$2
+
+python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_habitat/${EXP_NAME}.yaml \
+    datasets=synsin_habitat \
+    model=mesh_renderer \
+    env.save_dir=./save/synsin_habitat/${EXP_NAME} \
+    checkpoint.resume=True checkpoint.resume_best=True run_type=val ${ARGS}

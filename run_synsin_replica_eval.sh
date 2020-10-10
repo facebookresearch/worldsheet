eaae2c8fbde837f8f64d58159a061dfb1af345d8
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-09 21:34:09 -0700

    add both synsin habitat mp3d val and test

--- run_synsin_replica_eval.sh
+++ run_synsin_replica_eval.sh
@@ -7,5 +7,5 @@ python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_habitat/${
     env.save_dir=./save/synsin_habitat/${EXP_NAME} \
     dataset_config.synsin_habitat.multiview_data_dir=/checkpoint/ronghanghu/neural_rendering_datasets/synsin_replica \
     dataset_config.synsin_habitat.multiview_image_dir=/checkpoint/ronghanghu/neural_rendering_datasets/synsin_replica \
-    dataset_config.synsin_habitat.annotations.test="['synsin_habitat/defaults/annotations/imdb_replica_val.npy']" \
+    dataset_config.synsin_habitat.annotations.test="['synsin_habitat/defaults/annotations/imdb_replica_test.npy']" \
     checkpoint.resume=True checkpoint.resume_best=True run_type=test ${ARGS}

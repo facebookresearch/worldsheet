d38602a0714d614aedcf0105ecb48fb7bb1b29e9
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-12 10:12:24 -0700

    add notebook to visualize SynSin setting

---
+++ run_synsin_habitat/visualize_synsin_mp3d_test_jitter_angle20_iter.sh
@@ -0,0 +1,12 @@
+EXP_NAME=$1
+ITER=$2
+ARGS=$3
+
+python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_habitat/${EXP_NAME}.yaml \
+    datasets=synsin_habitat \
+    model=mesh_renderer \
+    env.save_dir=./save/synsin_habitat/${EXP_NAME} \
+    model_config.mesh_renderer.save_forward_results=True \
+    model_config.mesh_renderer.forward_results_dir=save/visualization_synsin_habitat/${EXP_NAME}/${ITER}/mp3d_test_jitter_angle20 \
+    dataset_config.synsin_habitat.annotations.test="['synsin_habitat/defaults/annotations/imdb_mp3d_test_jitter_angle20.npy']" \
+    checkpoint.resume_file=save/synsin_habitat/${EXP_NAME}/models/model_${ITER}.ckpt run_type=test ${ARGS}

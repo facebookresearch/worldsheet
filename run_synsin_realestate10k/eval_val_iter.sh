8c2295cf9c61828ff69cf5971df6fad6126e25a9
Ronghang Hu <ronghang.hu@gmail.com>
2020-10-16 20:12:05 -0700

    additional running scripts

---
+++ run_synsin_realestate10k/eval_val_iter.sh
@@ -0,0 +1,8 @@
+EXP_NAME=$1
+ARGS=$2
+
+python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_realestate10k/${EXP_NAME}.yaml \
+    datasets=synsin_realestate10k \
+    model=mesh_renderer \
+    env.save_dir=./save/synsin_realestate10k/${EXP_NAME} \
+    checkpoint.resume_file=save/synsin_realestate10k/${EXP_NAME}/models/model_${ITER}.ckpt run_type=test ${ARGS}

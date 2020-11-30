b3b4177e78ff915e430da56c270fa1eb345e41f9
Ronghang Hu <ronghang.hu@gmail.com>
2020-11-29 19:11:28 -0800

    more fixup

--- run_synsin_realestate10k/eval_val_iter.sh
+++ run_synsin_realestate10k/eval_val_iter.sh
@@ -6,5 +6,5 @@ python mmf_cli/run.py config=projects/neural_rendering/configs/synsin_realestate
     datasets=synsin_realestate10k \
     model=mesh_renderer \
     env.save_dir=./save/synsin_realestate10k/${EXP_NAME} \
-    checkpoint.resume_file=save/synsin_realestate10k/${EXP_NAME}/models/model_${ITER}.ckpt run_type=test ${ARGS}
+    checkpoint.resume_file=save/synsin_realestate10k/${EXP_NAME}/models/model_${ITER}.ckpt run_type=val ${ARGS}
 echo "exp:" ${EXP_NAME}
